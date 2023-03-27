230a77708ff78191fd8df295a05c6257cc884ca3
Enoque Leal <enoque.fsleal@sp.senac.br>
2023-03-27 16:54:49 +0000

    upload da primeira versao do projeto

---
+++ app/src/main/java/br/com/carsoft/servlet/CreateCarServlet.java
@@ -0,0 +1,24 @@
+package br.com.carsoft.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-car")
+public class CreateCarServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String carName = request.getParameter("car-name");
+
+        System.out.println(carName);
+
+        request.getRequestDispatcher("index.html").forward(request, response);
+
+    }
+
+}
---
+++ app/src/main/webapp/index.html
@@ -0,0 +1,15 @@
+<html>
+<body>
+<h2>Create Car</h2>
+
+<form action="/create-car" method="post">
+
+    <label>Car Name</label>
+    <input type="text" name="car-name" id="car-name">
+
+    <button type="submit">Register</button>
+
+</form>
+
+</body>
+</html>
